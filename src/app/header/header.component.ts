45c3ae2936b64aeb387dafddefaf745623262266
Gerard2par2 <floogles71@gmail.com>
2023-12-23 14:23:51 +0100

    use router when header link is clicked

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppContextService } from '../shared/services/app-context.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,11 @@ import { AppContextService } from '../shared/services/app-context.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  public constructor(public readonly appContextService: AppContextService) { }
+  public constructor(
+    public readonly appContextService: AppContextService,
+    private readonly router: Router) { }
+
+  public linkClicked(route: string[]): void {
+    this.router.navigate(route);
+  }
 }
